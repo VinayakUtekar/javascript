22367872fd0619d3cdf83d0af0e57f7dd45aa816
Vinayak Utekar <vinayakvutekar15@gmail.com>
2024-08-20 16:00:07 +0530

    Day5 & Day6

--- Day6/App.js
+++ Day6/App.js
@@ -11,8 +11,15 @@ console.log(100 * "5"); // string is converted to number
 console.log("100" / 10); //same
 
 //Example of explicit typecasting
-let x = '100';
+let x = '100.423';
 console.log(typeof x);
 
 //parseInt()->inbuilt method used to convert string into number
-parseInt(x);
+const r = parseInt(x);
+console.log(r);
+console.log(typeof r);
+
+//parseFloat()-> inbuilt method used to convert decimal string into number
+let r1 = parseFloat(x);
+console.log(r);
+console.log(typeof r);
