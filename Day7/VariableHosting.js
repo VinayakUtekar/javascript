c4d2e1b583bf873b5ac28cefe26e0c0a026f61a6
Vinayak Utekar <vinayakvutekar15@gmail.com>
2024-08-21 16:12:08 +0530

    Day7

---
+++ Day7/VariableHosting.js
@@ -0,0 +1,15 @@
+// Variable Hosting
+// console.log(b); //!undefined
+// console.log(a); //throws error
+
+let a = 50;
+var b = 90;
+let c = 68;
+
+console.log(a,b,c);
+
+demo();
+function demo(){
+    console.log("Demo Function");
+    return 200;
+}
