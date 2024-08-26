75c05c343cc82ca0b68954cb6357b7a19957ff63
Vinayak Utekar <vinayakvutekar15@gmail.com>
2024-08-26 16:57:15 +0530

    Day10

--- Day10/ClassConstructor.js
+++ Day10/ClassConstructor.js
@@ -9,23 +9,38 @@ class Employee {
   //inside the class if we are going to define any
   //varaible or property we are not supposed to pass var,let and const keywords
   //if we are going to pass any function then we are not suppose to use the function keyword
-  name = "Sachin";
+  static name = "Sachin";
   constructor(name, id) {
     this.eName = name;
     this.eId = id;
   }
-  demo(a, b) {
+  static demo(a, b) {
     //we can use var,let,const only inside the function inside a class
     var x = 10;
+    console.log(a, b);
     console.log("Hello World");
+    return x;
+  }
+  f1() {
+    console.log("non-static method");
+    console.log(this.Id);
   }
 }
 
 var e1 = new Employee("Rohit", 45); //constuctor call
 console.log(e1);
+console.log(e1.eName);
+console.log(e1.eId);
 
 var e2 = new Employee("Virat", 18);
 console.log(e2);
+console.log(e2.eName);
+console.log(e3.eId);
 
 var e3 = new Employee("Jaspreet", 93);
 console.log(e3);
+console.log(e3.eName);
+console.log(e3.eId);
+
+console.log(Employee.demo(100, 200));
+console.log(Employee.name);
